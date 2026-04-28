e0cc5fdf77015394a1f46e77c148a2eef6c59165
kavya <kurmalakavya123@gmail.com>
2026-04-28 13:28:27 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,11 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
